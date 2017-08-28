567e9c012fdc05885fe029a3b2f06885104a1b28
Diwas Sharma <diwasblack@gmail.com>
2017-08-28 13:56:06 -0500

    Using seperate implementation for DDQN

--- ddqn.py
+++ ddqn.py
@@ -1,108 +1,110 @@
-import random
-from collections import deque
-
 import gym
 import numpy as np
-
+import random
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
-from keras import backend as K
 
-EPISODES = 5000
+from collections import deque
 
 
-class DQNAgent:
-    def __init__(self, state_size, action_size):
-        self.state_size = state_size
-        self.action_size = action_size
-        self.memory = deque(maxlen=10000)
-        self.gamma = 0.95    # discount rate
-        self.epsilon = 1.0  # exploration rate
+class DQN:
+    def __init__(self, env):
+        self.env = env
+        self.memory = deque(maxlen=2000)
+
+        self.gamma = 0.85
+        self.epsilon = 1.0
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
-        self.learning_rate = 0.00025
-        self.model = self._build_model()
-        self.target_model = self._build_model()
-        self.update_target_model()
+        self.learning_rate = 0.005
+        self.tau = .125
 
-    def _huber_loss(self, target, prediction):
-        # sqrt(1+error^2)-1
-        error = prediction - target
-        return K.mean(K.sqrt(1+K.square(error))-1, axis=-1)
+        self.model = self.create_model()
+        self.target_model = self.create_model()
 
-    def _build_model(self):
-        # Neural Net for Deep-Q learning Model
+    def create_model(self):
         model = Sequential()
-        model.add(Dense(512, input_dim=self.state_size, activation='relu'))
-        model.add(Dense(256, activation='relu'))
-        model.add(Dense(512, activation='relu'))
-        model.add(Dense(self.action_size, activation='linear'))
-        model.compile(loss=self._huber_loss,
-                      optimizer=Adam(lr=self.learning_rate))
+        state_shape = self.env.observation_space.shape
+        model.add(Dense(24, input_dim=state_shape[0], activation="relu"))
+        model.add(Dense(48, activation="relu"))
+        model.add(Dense(24, activation="relu"))
+        model.add(Dense(self.env.action_space.n))
+        model.compile(
+            loss="mean_squared_error", optimizer=Adam(lr=self.learning_rate))
         return model
 
-    def update_target_model(self):
-        # copy weights from model to target_model
-        self.target_model.set_weights(self.model.get_weights())
-
-    def remember(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))
-
     def act(self, state):
-        if np.random.rand() <= self.epsilon:
-            return random.randrange(self.action_size)
-        act_values = self.model.predict(state)
-        return np.argmax(act_values[0])  # returns action
+        self.epsilon *= self.epsilon_decay
+        self.epsilon = max(self.epsilon_min, self.epsilon)
+        if np.random.random() < self.epsilon:
+            return self.env.action_space.sample()
+        return np.argmax(self.model.predict(state)[0])
 
-    def replay(self, batch_size):
-        minibatch = random.sample(self.memory, batch_size)
-        for state, action, reward, next_state, done in minibatch:
-            target = self.model.predict(state)
+    def remember(self, state, action, reward, new_state, done):
+        self.memory.append([state, action, reward, new_state, done])
+
+    def replay(self):
+        batch_size = 32
+        if len(self.memory) < batch_size:
+            return
+
+        samples = random.sample(self.memory, batch_size)
+        for sample in samples:
+            state, action, reward, new_state, done = sample
+            target = self.target_model.predict(state)
             if done:
                 target[0][action] = reward
             else:
-                a = self.model.predict(next_state)[0]
-                t = self.target_model.predict(next_state)[0]
-                target[0][action] = reward + self.gamma * t[np.argmax(a)]
+                Q_future = max(self.target_model.predict(new_state)[0])
+                target[0][action] = reward + Q_future * self.gamma
             self.model.fit(state, target, epochs=1, verbose=0)
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
 
-    def load(self, name):
-        self.model.load_weights(name)
+    def target_train(self):
+        weights = self.model.get_weights()
+        target_weights = self.target_model.get_weights()
+        for i in range(len(target_weights)):
+            target_weights[
+                i] = weights[i] * self.tau + target_weights[i] * (1 - self.tau)
+        self.target_model.set_weights(target_weights)
 
-    def save(self, name):
-        self.model.save_weights(name)
+    def save_model(self, fn):
+        self.model.save(fn)
+
+
+def main():
+    env = gym.make("MountainCar-v0")
+
+    trials = 1000
+    trial_len = 500
+
+    # updateTargetNetwork = 1000
+    dqn_agent = DQN(env=env)
+    for trial in range(trials):
+        cur_state = env.reset().reshape(1, 2)
+        for step in range(trial_len):
+            action = dqn_agent.act(cur_state)
+            new_state, reward, done, _ = env.step(action)
+
+            new_state = new_state.reshape(1, 2)
+            dqn_agent.remember(cur_state, action, reward, new_state, done)
+
+            dqn_agent.replay(
+            )  # internally iterates default (prediction) model
+            dqn_agent.target_train()  # iterates target model
+
+            cur_state = new_state
+            if done:
+                break
+        if step >= 199:
+            print("Failed to complete in trial {}".format(trial))
+            if step % 10 == 0:
+                dqn_agent.save_model("trial-{}.model".format(trial))
+        else:
+            print("Completed in {} trials".format(trial))
+            dqn_agent.save_model("success.model")
+            break
 
 
 if __name__ == "__main__":
-    env = gym.make('MountainCar-v0')
-    state_size = env.observation_space.shape[0]
-    action_size = env.action_space.n
-    agent = DQNAgent(state_size, action_size)
-    done = False
-    batch_size = 256
-
-    for e in range(EPISODES):
-        state = env.reset()
-        state = np.reshape(state, [1, state_size])
-        total_reward = 0
-        for time in range(500):
-            # env.render()
-            action = agent.act(state)
-            next_state, reward, done, _ = env.step(action)
-            next_state = np.reshape(next_state, [1, state_size])
-            agent.remember(state, action, reward, next_state, done)
-            state = next_state
-            total_reward += reward
-            if done:
-                agent.update_target_model()
-                if e % 500 == 0 or total_reward > -200:
-                    print("episode: {}/{}, score: {}, e: {:.2}".format(
-                        e, EPISODES, total_reward, agent.epsilon))
-                break
-        if len(agent.memory) > batch_size:
-            agent.replay(batch_size)
-        # if e % 10 == 0:
-        #     agent.save("./save/cartpole.h5")
+    main()
